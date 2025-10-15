c373d79a00ba401d76d234d2f3174b3e29908779
Ohaniana <Ohaniana@go.stockton.edu>
2025-10-15 16:04:25 -0400

    working on my branch

--- lab5.py
+++ lab5.py
@@ -10,3 +10,30 @@ from skimage.feature import hog
 from skimage import exposure
 
 #testing
+
+data_path = ".data/cifar-10-batches-py"
+
+download_flag = not os.path.exists(data_path)
+
+transform = transforms.ToTensor()
+
+dataset = CIFAR10(
+    root="./data",
+    train=False,
+    download=download_flag,
+    transform=transform
+)
+
+N = 10
+imgs_gray = []
+labels = []
+
+for i in range(N):
+    x, y = dataset[i]
+    img_rgb = x.permute(1, 2, 0).numpy()
+    img_gray = rgb2gray(img_rbg)
+    imgs_gray.append(img_gray)
+    labels.append(dataset.classes[y])
+
+imgs_gray = np.stack(imgs_gray, axis=0)
+print("Loaded greyscale images shape:", imgs_gray.shape)
