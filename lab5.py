1140501c583fde8e1c6846ba482e0c22a1ff4c5f
ArekOhanian <ohaniana@go.stockton.edu>
2025-10-19 19:31:12 -0400

    PLugged in out dataset to the code I wrote before

--- lab5.py
+++ lab5.py
@@ -2,27 +2,26 @@ import numpy as np
 import os
 import matplotlib.pyplot as plt
 
-from torchvision.datasets import CIFAR10
+from torchvision.datasets import ImageFolder
 from torchvision import transforms
 
 from skimage.color import rgb2gray
 from skimage.feature import hog
 from skimage import exposure
+from skimage.transform import resize
 
 #fetching and grayscaling the images from the dataset
 
-data_path = ".data/cifar-10-batches-py"
+data_path = "./fruit_images"
 
 download_flag = not os.path.exists(data_path)
 
-transform = transforms.ToTensor()
+transform = transforms.Compose([
+    transforms.Resize((64,64)),
+    transforms.ToTensor(),
+])
 
-dataset = CIFAR10(
-    root="./data",
-    train=False,
-    download=download_flag,
-    transform=transform
-)
+dataset = ImageFolder(root=data_path, transform=transform)
 
 N = 10
 imgs_gray = []
@@ -31,26 +30,27 @@ labels = []
 for i in range(N):
     x, y = dataset[i]
     img_rgb = x.permute(1, 2, 0).numpy()
-    img_gray = rgb2gray(img_rbg)
+    img_gray = rgb2gray(img_rgb)
     imgs_gray.append(img_gray)
     labels.append(dataset.classes[y])
 
-    imgs_gray = np.stack(imgs_gray, axis=0)
-    print("Loaded greyscale images shape:", imgs_gray.shape)
+imgs_gray = np.stack(imgs_gray, axis=0)
+print("Loaded greyscale images shape:", imgs_gray.shape)
+
 
 #This method is for showing 10 of the images and their label
-def show_images_grid(imgs, labels=None, rows=2, cols=5, title="CIFAR-10 Samples", display_size = (32,32)):
+def show_images_grid(imgs, labels=None, rows=2, cols=5, title="Our Dataset Samples", display_size = (32,32)):
     fig, axes = plt.subplots(rows,cols, figsize=(12,5))
-    fig.subtitle(title, fontsize=14)
+    fig.suptitle(title, fontsize=14)
     for i in range(rows * cols):
         ax = axes[i// cols, i % cols]
         ax.axis('off')
         if i < len(imgs):
             img_resized = resize(imgs[i], display_size, anti_aliasing=True)
             if img_resized.ndim == 2:
-                ax.imshow(img_resized, interpolation="nearest")
+                ax.imshow(img_resized, cmap='gray')
             if labels is not None:
-                ax.set_title(labels[i], fonstsize=9)
+                ax.set_title(labels[i], fontsize=9)
     plt.tight_layout()
     plt.show()
 
@@ -68,7 +68,7 @@ for i in range(len(imgs_gray)):
         pixels_per_cell=(8,8),
         cells_per_block=(2,2),
         block_norm="L2-Hys",
-        visualization=True,
+        visualize=True,
     )
     print(feat.shape)
     features_list.append(feat)
@@ -79,13 +79,13 @@ print("HOG feature length (first image):", len(features_list[0]))
 
 #displaying the HOGed images
 def show_hog_grid(hog_images, rows=2, cols=5, title="HOG Renderings"):
-    fig, axes = plt.subplots(rows, cols, figuresize=(12, 5))
-    fig.subtitle(title, fontsize=14)
+    fig, axes = plt.subplots(rows, cols, figsize=(12, 5))
+    fig.suptitle(title, fontsize=14)
     for i in range (rows*cols):
         ax = axes[i // cols, i % cols]
         ax.axis('off')
         if i < len(hog_images):
-            ax.imshow(hog_images[1], cmap="gray")
+            ax.imshow(hog_images[i], cmap="gray")
     plt.tight_layout()
     plt.show()
 
