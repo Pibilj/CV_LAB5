88ba4a62a166c59608df795745eb367929f06fca
ArekOhanian <ohaniana@go.stockton.edu>
2025-10-21 15:42:55 -0400

    KNN and training test split implemented

--- lab5.py
+++ lab5.py
@@ -9,6 +9,9 @@ from skimage.color import rgb2gray
 from skimage.feature import hog
 from skimage import exposure
 from skimage.transform import resize
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import classification_report, accuracy_score
 
 #fetching and grayscaling the images from the dataset
 
@@ -23,7 +26,7 @@ transform = transforms.Compose([
 
 dataset = ImageFolder(root=data_path, transform=transform)
 
-N = 10
+N = len(dataset)
 imgs_gray = []
 labels = []
 
@@ -90,3 +93,26 @@ def show_hog_grid(hog_images, rows=2, cols=5, title="HOG Renderings"):
     plt.show()
 
 show_hog_grid(hog_imgs[:10], rows = 2, cols = 5)
+
+#here is stratisfying the data into a 80 train 20 test split
+labels = []
+for i in range(N):
+    _, lab = dataset[i]
+    labels.append(lab)
+labels = np.array(labels)
+
+x_train, x_test, y_train, y_test = train_test_split(
+    features_list,
+    labels,
+    test_size=0.2,
+    stratify=labels,
+    random_state=42
+)
+
+#here is the KNN classifier
+knn= KNeighborsClassifier(n_neighbors=3)
+knn.fit(x_train, y_train)
+y_pred = knn.predict(x_test)
+
+print("Accuracy:", accuracy_score(y_test, y_pred))
+print(classification_report(y_test,y_pred))
