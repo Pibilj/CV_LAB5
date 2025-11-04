b032ea41681933688b936312b3c71a1102c05ffd
ArekOhanian <ohaniana@go.stockton.edu>
2025-11-04 14:30:33 -0500

    3.1 is compleated

--- lab5_part3.py
+++ lab5_part3.py
@@ -5,6 +5,7 @@ import torch
 
 from torchvision.datasets import ImageFolder
 from torchvision import transforms
+from torch.utils.data import TensorDataset, DataLoader
 
 from skimage.color import rgb2gray
 from skimage.feature import hog
@@ -32,7 +33,7 @@ transform = transforms.Compose(
 
 dataset = ImageFolder(root=data_path, transform=transform)
 
-#Writting the images and labels to stacks
+#Stacking into tensors
 labels = []
 images = []
 for i in range(len(dataset)):
@@ -42,3 +43,31 @@ for i in range(len(dataset)):
 
 labels = torch.tensor(labels)
 images = torch.stack(images)
+
+#converting to a 1D Vector
+images_flat = images.view(images.size(0), -1)
+
+#Making the stratisized split
+x_train, x_test, y_train, y_test = train_test_split(
+    images_flat.numpy(),
+    labels.numpy(),
+    test_size = 0.2,
+    stratify = labels.numpy(),
+    random_state = 42
+)
+
+
+#Converting x & y test and train back into tensors
+x_train_tensor = torch.tensor(x_train, dtype=torch.float32)
+y_train_tensor = torch.tensor(y_train, dtype=torch.long)
+x_test_tensor = torch.tensor(x_test, dtype=torch.float32)
+y_test_tensor = torch.tensor(y_test, dtype=torch.long)
+
+#creating the test and train data sets
+train_dataset = TensorDataset(x_train_tensor, y_train_tensor)
+test_dataset = TensorDataset(x_test_tensor, y_test_tensor)
+
+#Creating Data loaders
+train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=64, shuffle=True)
+
