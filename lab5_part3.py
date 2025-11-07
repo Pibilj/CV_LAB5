c952aeaa6b053534dcb4a4173e02d0c148ccbdbf
ArekOhanian <ohaniana@go.stockton.edu>
2025-11-07 14:59:18 -0500

    Created the model for the CNN it is dynamic so you can make up to 6 for the testing requirements with it

--- lab5_part3.py
+++ lab5_part3.py
@@ -2,6 +2,9 @@ import numpy as np
 import os
 import matplotlib.pyplot as plt
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
 
 from torchvision.datasets import ImageFolder
 from torchvision import transforms
@@ -20,9 +23,12 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 data_path = os.path.join(script_dir, "fruit_images")
 
 transform = transforms.Compose(
-    [
+    [   
         transforms.Resize((64,64)),
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomRotation(10),
         transforms.ToTensor(),
+    
     ]
 )
 
@@ -39,12 +45,11 @@ for i in range(len(dataset)):
 labels = torch.tensor(labels)
 images = torch.stack(images)
 
-#converting to a 1D Vector
-images_flat = images.view(images.size(0), -1)
+
 
 #Making the stratisized split
 x_train, x_test, y_train, y_test = train_test_split(
-    images_flat.numpy(),
+    images.numpy(),
     labels.numpy(),
     test_size = 0.2,
     stratify = labels.numpy(),
@@ -63,6 +68,56 @@ train_dataset = TensorDataset(x_train_tensor, y_train_tensor)
 test_dataset = TensorDataset(x_test_tensor, y_test_tensor)
 
 #Creating Data loaders
-train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
-test_loader = DataLoader(test_dataset, batch_size=64, shuffle=True)
+train_loader = DataLoader(train_dataset, batch_size=100, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=100, shuffle=False)
 
+# Defining the CNN architecture
+class CNN(nn.Module):
+    def __init__(self, N, M, num_classes=5, input_shape=(3,64,64)):
+        super(CNN, self).__init__()
+
+        # making a module list for the convolution layers so we can have a different amount of them
+        self.convs = nn.ModuleList()
+        in_channels = input_shape[0]
+
+        #Here we dynamically create the ammount of convolution layers we want for the NN
+        for i in N:
+            conv_layer = nn.Conv2d(in_channels, i, kernel_size=3, padding=1)
+            self.convs.append(conv_layer)
+            in_channels = i
+        
+        self.pool = nn.MaxPool2d(2,2)
+        self.dropout = nn.Dropout(0.25)
+
+        with torch.no_grad():
+            x = torch.zeros(1, *input_shape)
+            for conv in self.convs:
+                x = self.pool(F.relu(conv(x)))
+            self.flattened_size = x.view(1,-1).size(1)
+        
+        self.fcs = nn.ModuleList()
+        in_features = self.flattened_size
+        for out_features in M:
+            self.fcs.append(nn.Linear(in_features, out_features))
+            in_features = out_features
+
+        self.output = nn.Linear(in_features, num_classes)
+    def forward(self, x):
+        for conv in self.convs:
+            x = self.pool(F.relu(conv(x)))
+        
+        x = x.view(x.size(0), -1)
+
+        for fc in self.fcs:
+            x = self.dropout(F.relu(fc(x)))
+        
+        x = self.dropout(x)
+        return x
+model = CNN(N=[16,32,64], M=[500])
+
+print(model)
+
+#For the optimizer and lossfunction
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr = 0.01)
+scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer=optimizer, patience=5)
